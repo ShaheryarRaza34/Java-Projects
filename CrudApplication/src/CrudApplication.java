587e0a027689568bf7bb5a090331df23fbb026da
SharyR <shaheryarraza34@gmail.com>
2022-01-03 17:25:54 +0500

    Add files via upload

---
+++ CrudApplication/src/CrudApplication.java
@@ -0,0 +1,30 @@
+import java.sql.*;
+public class CrudApplication {
+	
+		
+/*	try {
+	Class.forName("oracle.jdbc.driver.OracleDriver");  
+	  
+	//step2 create  the connection object  
+	Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl","system","Shary1769175!");  
+	  
+	//step3 create the statement object  
+	Statement stmt=con.createStatement();  
+	  
+	//step4 execute query  
+	ResultSet rs=stmt.executeQuery("SELECT * FROM SUPPLIER");  
+	while(rs.next())  
+	System.out.println(rs.getString("SUPNR")); 
+	  
+	//step5 close the connection object  
+	con.close();  
+	  
+	}catch(Exception e)
+	{ 
+		System.out.println(e);
+	}  
+	*/  
+}
+  
+
+
